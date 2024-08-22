7b86bd1befd0923555370ecf9e8f7b05f86fcb9a
GeorgeAbs <g.k.yudin@gmail.com>
2024-08-22 15:47:21 +0200

    Add project files.

---
+++ PictureConverter/PictureConverter.cs
@@ -0,0 +1,39 @@
+﻿namespace PictureConverter
+{
+    public class PictureConverter
+    {
+        public static bool ConvertPictureTranslucentPixelsToTransparent(string pictureFullPath)
+        {
+            try
+            {
+                using Image<Rgba32> image = Image.Load<Rgba32>(pictureFullPath);
+
+                image.ProcessPixelRows(accessor =>
+                {
+
+                    for (int y = 0; y < accessor.Height; y++)
+                    {
+                        Span<Rgba32> pixelRow = accessor.GetRowSpan(y);
+
+                        for (int x = 0; x < pixelRow.Length; x++)
+                        {
+                            ref Rgba32 pixel = ref pixelRow[x];
+                            if (pixel.A < 10.0)
+                            {
+                                pixel.A = 0;
+                            }
+                        }
+                    }
+                });
+                image.Save(pictureFullPath);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            
+        }
+    }
+}
